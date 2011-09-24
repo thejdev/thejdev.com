c39381c22edaafcb2aca58b6225edcb3a4790009
Jaidev Vasudevan <jaidev.v@gmail.com>
2011-09-24 18:00:47 +1100

    first update

---
+++ thejdev.com/thejdev.com_files/thejdev.com.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20110924
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,205),url:'thejdev.com_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'thejdev.com_files/stroke_1.png'},{rect:new IWRect(1,-1,422,2),url:'thejdev.com_files/stroke_2.png'},{rect:new IWRect(423,-1,2,2),url:'thejdev.com_files/stroke_3.png'},{rect:new IWRect(423,1,2,205),url:'thejdev.com_files/stroke_4.png'},{rect:new IWRect(423,206,2,2),url:'thejdev.com_files/stroke_5.png'},{rect:new IWRect(1,206,422,2),url:'thejdev.com_files/stroke_6.png'},{rect:new IWRect(-1,206,2,2),url:'thejdev.com_files/stroke_7.png'}],new IWSize(424,207))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('thejdev.com_files/thejdev.comMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');IMpreload('thejdev.com_files','shapeimage_1','0');IMpreload('thejdev.com_files','shapeimage_2','0');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
