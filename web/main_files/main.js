50f82a83147ce2e34b2c42b3db5302f6b0488054
Jaidev Vasudevan <github@z.thejdev.com>
2011-10-04 07:35:04 +1100

    Renamed pages

---
+++ web/main_files/main.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20111004
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,205),url:'main_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'main_files/stroke_1.png'},{rect:new IWRect(1,-1,422,2),url:'main_files/stroke_2.png'},{rect:new IWRect(423,-1,2,2),url:'main_files/stroke_3.png'},{rect:new IWRect(423,1,2,205),url:'main_files/stroke_4.png'},{rect:new IWRect(423,206,2,2),url:'main_files/stroke_5.png'},{rect:new IWRect(1,206,422,2),url:'main_files/stroke_6.png'},{rect:new IWRect(-1,206,2,2),url:'main_files/stroke_7.png'}],new IWSize(424,207))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('main_files/mainMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');IMpreload('main_files','shapeimage_1','0');IMpreload('main_files','shapeimage_2','0');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
